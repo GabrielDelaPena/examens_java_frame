4f305c03c13f80227ac7ae4d531cd545d922fcc3
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2022-06-13 09:21:42 +0200

    first commit

---
+++ src/main/java/be/ehb/examens/ExamensApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.examens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamensApplication.class, args);
+    }
+
+}
