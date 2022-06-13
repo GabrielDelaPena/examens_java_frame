2b2ce6eb3045fa44ea43eb514a5c28f1dac8d822
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2022-06-13 09:57:38 +0200

    Opvragen van alle verhuren

--- src/main/java/be/ehb/examens/controller/VerhuurController.java
+++ src/main/java/be/ehb/examens/controller/VerhuurController.java
@@ -1,9 +1,25 @@
 package be.ehb.examens.controller;
 
+import be.ehb.examens.dao.VerhuurDao;
+import be.ehb.examens.entities.Verhuur;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/rentals")
+@RequestMapping("/api/locations")
 public class VerhuurController {
+
+    private VerhuurDao verhuurDao;
+
+    @Autowired
+    public VerhuurController(VerhuurDao verhuurDao) {
+        this.verhuurDao = verhuurDao;
+    }
+
+    @GetMapping
+    public Iterable<Verhuur> getAllVerhuren() {
+        return verhuurDao.findAll();
+    }
 }
