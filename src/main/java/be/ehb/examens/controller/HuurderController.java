69090e6f2d6726f9ef94d156fc7f4567dc5448c6
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2022-06-13 10:18:12 +0200

    Aanmaken van een nieuwe huurder, gebruik query parameters

--- src/main/java/be/ehb/examens/controller/HuurderController.java
+++ src/main/java/be/ehb/examens/controller/HuurderController.java
@@ -1,9 +1,45 @@
 package be.ehb.examens.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import be.ehb.examens.dao.HuurderDao;
+import be.ehb.examens.entities.Huurder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/renters")
+@RequestMapping("/api/customers")
 public class HuurderController {
+
+    private HuurderDao huurderDao;
+
+    @Autowired
+    public HuurderController(HuurderDao huurderDao) {
+        this.huurderDao = huurderDao;
+    }
+
+    @GetMapping
+    public Iterable<Huurder> getAllHuurders() {
+        return huurderDao.findAll();
+    }
+
+    @PostMapping("/new")
+    public HttpStatus addNewHuurder(
+            @Valid @RequestParam("voornaam") String voornaam,
+            @Valid @RequestParam("achternaam") String achternaam,
+            @Valid @RequestParam("telefoonnr") long telefoonnr,
+            @Valid @RequestParam("email") String email
+    ) {
+        Huurder huurder = new Huurder();
+        huurder.setVoornaam(voornaam);
+        huurder.setAchternaam(achternaam);
+        huurder.setTelefoonnr(telefoonnr);
+        huurder.setEmail(email);
+
+        huurderDao.save(huurder);
+
+        return HttpStatus.ACCEPTED;
+    }
 }
